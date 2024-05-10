f94c1c0644adc4aa824226039047bd8a8deef4b8
sabrinadizam21 <sabrinadizam21@gmail.com>
2024-05-10 23:13:10 +0700

    add login test case

---
+++ Tests/CHS4_Logout.py
@@ -0,0 +1,42 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from Pages.loginPage import LoginPage
+from Pages.sidebar import Sidebar
+
+class Logout(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+
+    def test_logout_success(self):
+        driver = self.driver
+        driver.get('https://katalon-demo-cura.herokuapp.com/')
+
+        sidebar = Sidebar(driver)
+        sidebar.click_menu()
+        sidebar.click_login()
+
+        loginPage = LoginPage(driver)
+        loginPage.login_valid('John Doe', 'ThisIsNotAPassword')
+
+        sidebar.click_menu()
+        sidebar.click_logout()
+
+        # Validation
+        sidebar.click_menu()
+        driver.find_element(By.XPATH, sidebar.login_menu_xpath).is_displayed()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
