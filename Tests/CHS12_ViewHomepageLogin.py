bc73506ceb1235d09c4b5322188119a041db9d99
sabrinadizam21 <sabrinadizam21@gmail.com>
2024-05-14 22:37:15 +0700

    add homepage & profile test cases

---
+++ Tests/CHS12_ViewHomepageLogin.py
@@ -0,0 +1,45 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from Pages.sidebar import Sidebar
+from Pages.homePage import Homepage
+from Pages.loginPage import LoginPage
+
+class HomepageLogin(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+
+    def test_view_homepage_after_login(self):
+        driver = self.driver
+        driver.get('https://katalon-demo-cura.herokuapp.com/')
+
+        sidebar = Sidebar(driver)
+        sidebar.click_menu()
+        sidebar.click_login()
+
+        loginPage = LoginPage(driver)
+        loginPage.login_valid('John Doe', 'ThisIsNotAPassword')
+        time.sleep(1)
+
+        sidebar = Sidebar(driver)
+        sidebar.click_menu()
+        sidebar.click_home()
+
+        # Validation
+        homepage = Homepage(driver)
+        driver.find_element(By.ID, homepage.make_appointment_btn_id).is_displayed()
+        driver.find_element(By.ID, homepage.make_appointment_section_id).is_displayed()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Tests/CHS13_ViewProfile.py
@@ -0,0 +1,42 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from Pages.sidebar import Sidebar
+from Pages.loginPage import LoginPage
+
+class ViewProfile(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+
+    def test_view_profile_page(self):
+        driver = self.driver
+        driver.get('https://katalon-demo-cura.herokuapp.com/')
+
+        sidebar = Sidebar(driver)
+        sidebar.click_menu()
+        sidebar.click_login()
+
+        loginPage = LoginPage(driver)
+        loginPage.login_valid('John Doe', 'ThisIsNotAPassword')
+        time.sleep(1)
+
+        sidebar = Sidebar(driver)
+        sidebar.click_menu()
+        sidebar.click_profile()
+
+        # Validation
+        driver.find_element(By.XPATH, '//h2[(text = "Profile" or . = "Profile")]').is_displayed()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
